f2d9439c51a68b7e6274f664bb0695800098c928
ManuelAlrod <manuelalmengor32@gmail.com>
2023-10-18 23:13:05 -0500

    Edite las views para que mostrar los productos y categorias desde el front-end, agregue un html para categoria, se creo eliminar la categoria

--- Prueba/prueba/main/urls.py
+++ Prueba/prueba/main/urls.py
@@ -6,10 +6,12 @@ app_name = "main"
 urlpatterns = [
 	path('', views.home, name="home"),
     path('login-Registro/', views.registrar_usuario, name="login-Registro"),
-    path('productos/', views.productos, name="productos"),
+    path('productos/', views.productosvistas, name="productos"),
     path('contactos/', views.contacto, name="contactos"),
+    path('categorias/', views.categoriavistas, name="categorias"),
     path('agregar-productos/', views.agregarproducto, name="agregar-productos"),
     path('detalles-productos/', views.detallesproductos, name="detalles-productos"),
     path('agregar-categoria/', views.agregarcategoria, name="agregar-categoria"),
     path('editar-categoria/', views.editarcategoria, name='editar-categoria'),
+    path('eliminar-categoria/', views.eliminarcategoria , name="eliminar-categoria"),
 	]
